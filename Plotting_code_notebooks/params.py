3b1aa4295a152fbdc0a443a794cf67e2729549d9
RossHart <ross.hart@nottingham.ac.uk>
2015-09-01 12:38:03 +0100

    Have a 1D KDE optimisation attempt using k-fold cross validation

--- Plotting_code_notebooks/params.py
+++ Plotting_code_notebooks/params.py
@@ -1,10 +1,11 @@
 gz_dir =  "../../fits/" # Directory with debiased and raw data.
 
-filename = "Volume_limited_sample_Baldry_w_bd.fits" # FITS file of the 
+#filename = "Volume_limited_sample_Baldry_w_bd.fits" # FITS file of the 
 # volume-limited sample.
+filename = "data_vl.fits"
 
-deb_vals = "debiased.npy" # Numpy table containing the debiased values
-
+#deb_vals = "debiased.npy" # Numpy table containing the debiased values
+deb_vals = "debiased_vl_t11_arms_number.fits"
 #---------------------------------------------------------------------
 
 sf_dir = "../../../smpy-ross/" # smpy output file directory
